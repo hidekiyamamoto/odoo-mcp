6f45becb2959d8b85e985b92658e2b249ec659d9
Hideki Andrea Yamamoto <hideki.yamamoto@gmail.com>
2026-05-07 00:50:23 +0000

    feat: add marketplace thumbnail

--- perfect_odoo_mcp/__manifest__.py
+++ perfect_odoo_mcp/__manifest__.py
@@ -13,6 +13,7 @@
         "views/res_config_settings_views.xml",
     ],
     "images": [
+        "static/description/thumbnail.svg",
         "static/description/banner.svg",
     ],
     "installable": True,
